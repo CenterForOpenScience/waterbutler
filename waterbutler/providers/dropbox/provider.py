922c64f93f5591f244a58eab74e53302ae3e3c2e
Fitz Elliott <fitz@cos.io>
2015-10-07 13:51:45 -0400

    v1: implement pathsem validation for dropbox provider

--- waterbutler/providers/dropbox/provider.py
+++ waterbutler/providers/dropbox/provider.py
@@ -24,7 +24,23 @@ class DropboxProvider(provider.BaseProvider):
 
     @asyncio.coroutine
     def validate_v1_path(self, path, **kwargs):
-        return self.validate_path(path, **kwargs)
+        if path == '/':
+            return WaterButlerPath(path, prepend=self.folder)
+
+        implicit_folder = path.endswith('/')
+
+        resp = yield from self.make_request(
+            'GET', self.build_url('metadata', 'auto', self.folder + path),
+            expects=(200,),
+            throws=exceptions.MetadataError
+        )
+
+        data = yield from resp.json()
+        explicit_folder = data['is_dir']
+        if explicit_folder != implicit_folder:
+            raise exceptions.NotFoundError(str(path))
+
+        return WaterButlerPath(path, prepend=self.folder)
 
     @asyncio.coroutine
     def validate_path(self, path, **kwargs):
