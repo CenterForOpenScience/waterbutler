05c4da4667df7205a7caa15adc845889e3729fd8
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2015-10-22 20:31:39 -0200

    Remove unused settings variable

--- waterbutler/providers/sharelatex/settings.py
+++ waterbutler/providers/sharelatex/settings.py
@@ -6,5 +6,4 @@ except ImportError:
 config = settings.get('SHARELATEX_PROVIDER_CONFIG', {})
 
 BASE_URL = config.get('BASE_URL', 'http://sharelatex.com/api/v1')
-BASE_CONTENT_URL = config.get('BASE_CONTENT_URL', 'http://sharelatex.com/raw')
 VIEW_URL = config.get('VIEW_URL', 'http://sharelatex.com/')
