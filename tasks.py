978b552e987b1381b0324403fc85652e1f8af022
icereval <icereval@gmail.com>
2016-02-14 21:55:40 -0500

    patch certificate verification failure with rackspace services

--- tasks.py
+++ tasks.py
@@ -5,6 +5,28 @@ from invoke import task, run
 WHEELHOUSE_PATH = os.environ.get('WHEELHOUSE')
 
 
+def monkey_patch():
+    # Force an older cacert.pem from certifi v2015.4.28, prevents an ssl failure w/ identity.api.rackspacecloud.com.
+    #
+    # SubjectAltNameWarning: Certificate for identity.api.rackspacecloud.com has no `subjectAltName`, falling
+    # back to check for a `commonName` for now. This feature is being removed by major browsers and deprecated by
+    # RFC 2818. (See  https://github.com/shazow/urllib3/issues/497  for details.)
+    # SubjectAltNameWarning
+    import ssl
+    import certifi
+
+    _create_default_context = ssl.create_default_context
+
+    def create_default_context(purpose=ssl.Purpose.SERVER_AUTH, *, cafile=None, capath=None, cadata=None):
+        if cafile is None:
+            cafile = certifi.where()
+        return _create_default_context(purpose=purpose, cafile=cafile, capath=capath, cadata=cadata)
+    ssl.create_default_context = create_default_context
+
+
+monkey_patch()
+
+
 @task
 def wheelhouse(develop=False):
     req_file = 'dev-requirements.txt' if develop else 'requirements.txt'
