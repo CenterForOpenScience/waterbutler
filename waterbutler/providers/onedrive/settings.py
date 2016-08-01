0e18555d599ae74ece9375e7e7c8da69143b8c6f
Ryan Casey <rcasey@growingtechnologies.com>
2016-08-01 13:53:59 -0400

    fix settings to use config.get

--- waterbutler/providers/onedrive/settings.py
+++ waterbutler/providers/onedrive/settings.py
@@ -9,5 +9,5 @@ config = settings.get('ONEDRIVE_PROVIDER_CONFIG', {})
 BASE_URL = config.get('BASE_URL', 'https://api.onedrive.com/v1.0/drive/items/')
 BASE_CONTENT_URL = config.get('BASE_CONTENT_URL', 'https://api.onedrive.com/v1.0/drive/items/')
 BASE_ROOT_URL = config.get('BASE_ROOT_URL', 'https://api.onedrive.com/v1.0')
-ONEDRIVE_COPY_INTERATION_COUNT = 30
-ONEDRIVE_COPY_SLEEP_INTERVAL = 3
+ONEDRIVE_COPY_INTERATION_COUNT = config.get('ONEDRIVE_COPY_INTERATION_COUNT', 30)
+ONEDRIVE_COPY_SLEEP_INTERVAL = config.get('ONEDRIVE_COPY_SLEEP_INTERVAL', 3)
