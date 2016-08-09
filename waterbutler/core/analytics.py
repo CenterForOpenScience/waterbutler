7a954351c5ec36f0c44700c84083da597e45ca04
Fitz Elliott <fitz@cos.io>
2016-08-09 10:35:04 -0400

    analytics: start logging to public collection
     * Public payloads are sanitized versions of the private payloads.
    
     * Add some synthetic fields to both payloads for easier querying with Keen

--- waterbutler/core/analytics.py
+++ waterbutler/core/analytics.py
@@ -1,7 +1,9 @@
+import re
 import json
 import logging
 
 import aiohttp
+from geoip import geolite2
 
 import waterbutler
 from waterbutler import settings
@@ -11,13 +13,13 @@ logger = logging.getLogger(__name__)
 
 
 async def log_download(action, payload, request, api_version, size=0):
-    if settings.KEEN_PROJECT_ID is None or action not in ('download_file', 'download_zip', ):
+    if settings.KEEN_PRIVATE_PROJECT_ID is None or action not in ('download_file', 'download_zip', ):
         return
 
-    url = '{0}/{1}/projects/{2}/events/{3}'.format(settings.KEEN_API_BASE_URL,
-                                                   settings.KEEN_API_VERSION,
-                                                   settings.KEEN_PROJECT_ID,
-                                                   'file_access')
+    logger.info('Request IP is: {}'.format(request['ip']))
+    location = None
+    if request['ip'] and re.match('\d+\.\d+\.\d+\.\d+', request['ip']):  # needs IPv4 format
+        location = geolite2.lookup(request['ip'])
 
     referrer = request.pop('referrer', '')
     is_mfr_render = request.pop('is_mfr_render')
@@ -39,6 +41,10 @@ async def log_download(action, payload, request, api_version, size=0):
             'is_mfr_render': is_mfr_render,
         },
         'file': file_meta,
+        'anon': {
+            'continent': getattr(location, 'continent', None),
+            'country': getattr(location, 'country', None),
+        },
         'keen': {
             'addons': [
                 {
@@ -74,21 +80,48 @@ async def log_download(action, payload, request, api_version, size=0):
         },
     }
 
+    # synthetic fields to make Keen queries easier/prettier
     keen_payload['file']['full_path'] = '/'.join([
         '', file_meta['resource'], file_meta['provider'], file_meta['path'].lstrip('/')
     ])
+    keen_payload['file']['full_materialized'] = '/'.join([
+        '', file_meta['resource'], file_meta['provider'], file_meta['materialized'].lstrip('/')
+    ])
+    keen_payload['action']['subtype'] = 'view_file' if action == 'download_file' and is_mfr_render else action
 
-    serialized = json.dumps(keen_payload).encode('UTF-8')
+    collection = 'file_access'
+
+    # send the private payload
+    await _send_to_keen(keen_payload, collection, settings.KEEN_PRIVATE_PROJECT_ID,
+                        settings.KEEN_PRIVATE_WRITE_KEY, 'private')
+
+    # sanitize then send the public payload
+    for field in ('ip', 'headers', 'ua', ):
+        del keen_payload['request'][field]
+    for addon in keen_payload['keen']['addons']:
+        if addon['name'] in ('keen:ip_to_geo',):
+            keen_payload['keen']['addons'].remove(addon)
+    await _send_to_keen(keen_payload, collection, settings.KEEN_PUBLIC_PROJECT_ID,
+                        settings.KEEN_PUBLIC_WRITE_KEY, 'public')
+
+
+async def _send_to_keen(payload, collection, project_id, write_key, domain='private'):
+    serialized = json.dumps(payload).encode('UTF-8')
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': settings.KEEN_WRITE_KEY,
+        'Authorization': write_key,
     }
+    url = '{0}/{1}/projects/{2}/events/{3}'.format(settings.KEEN_API_BASE_URL,
+                                                   settings.KEEN_API_VERSION,
+                                                   project_id, collection)
 
     async with await aiohttp.request('POST', url, headers=headers, data=serialized) as resp:
         if resp.status == 201:
-            logger.info('Successfully logged download to Keen')
+            logger.info('Successfully logged download to {} collection in {} Keen'.format(
+                collection, domain
+            ))
         else:
-            raise Exception('Failed to log to Keen. Status: {} Error: {}'.format(
-                str(int(resp.status)), await resp.read()
+            raise Exception('Failed to log to {} collection in {} Keen. Status: {} Error: {}'.format(
+                collection, domain, str(int(resp.status)), await resp.read()
             ))
         return
