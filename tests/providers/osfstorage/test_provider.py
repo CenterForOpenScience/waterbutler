203405cd0344cf56f277849772359e89fe0721d8
Darion Cassel <darion.cassel@gmail.com>
2016-09-09 15:48:30 -0400

    Add test_download_without_auth

--- tests/providers/osfstorage/test_provider.py
+++ tests/providers/osfstorage/test_provider.py
@@ -18,6 +18,7 @@ from waterbutler.core.path import WaterButlerPath
 from waterbutler.providers.osfstorage import OSFStorageProvider
 from waterbutler.providers.osfstorage.settings import FILE_PATH_COMPLETE
 
+import pdb
 
 @pytest.fixture
 def file_content():
@@ -184,9 +185,29 @@ def file_lineage():
 
 @pytest.mark.asyncio
 @pytest.mark.aiohttpretty
-async def test_download(monkeypatch, provider_and_mock, osf_response, mock_path, mock_time):
+async def test_download_with_auth(monkeypatch, provider_and_mock, osf_response, mock_path, mock_time):
     provider, inner_provider = provider_and_mock
     params = {'user': 'cat'}
+    base_url = provider.build_url(mock_path.identifier, 'download', version=None, mode=None)
+    url, _, params = provider.build_signed_url('GET', base_url, params=params)
+
+    aiohttpretty.register_json_uri('GET', url, params=params, body=osf_response)
+
+    await provider.download(mock_path)
+
+    assert provider.make_provider.called
+    assert inner_provider.download.called
+
+    assert aiohttpretty.has_call(method='GET', uri=url, params=params)
+    provider.make_provider.assert_called_once_with(osf_response['settings'])
+    inner_provider.download.assert_called_once_with(path=WaterButlerPath('/test/path'), displayName='unrelatedpath')
+
+@pytest.mark.asyncio
+@pytest.mark.aiohttpretty
+async def test_download_without_auth(monkeypatch, provider_and_mock, osf_response, mock_path, mock_time):
+    provider, inner_provider = provider_and_mock
+    provider.auth = {} # Remove auth for test
+    params = {}
     base_url = provider.build_url(mock_path.identifier, 'download', version=None, mode=None)
     url, _, params = provider.build_signed_url('GET', base_url, params=params)
 
