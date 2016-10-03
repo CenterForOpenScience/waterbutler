575e1c2783cca90e545b74ea75ac21cf9fa0af75
icereval <icereval@gmail.com>
2016-10-02 20:07:31 -0400

    remote debugging

--- tasks.py
+++ tasks.py
@@ -78,6 +78,12 @@ def rabbitmq():
 def server():
     monkey_patch()
 
+    if os.environ.get('REMOTE_DEBUG', None):
+        import pydevd
+        # e.g. '127.0.0.1:5678'
+        remote_parts = os.environ.get('REMOTE_DEBUG').split(':')
+        pydevd.settrace(remote_parts[0], port=int(remote_parts[1]), suspend=False, stdoutToServer=True, stderrToServer=True)
+
     from waterbutler.server.app import serve
     serve()
 
