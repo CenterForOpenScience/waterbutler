b02485757a5481afd84cbf99b2d85b7255f4f396
TomBaxter <tkb608@gmail.com>
2016-10-18 13:43:48 -0400

    Update Figshare provider to Figshare v2 API [OSF-5599]

--- waterbutler/providers/figshare/settings.py
+++ waterbutler/providers/figshare/settings.py
@@ -3,5 +3,13 @@ from waterbutler import settings
 config = settings.child('FIGSHARE_PROVIDER_CONFIG')
 
 
-BASE_URL = config.get('BASE_URL', 'http://api.figshare.com/v1/my_data')
-VIEW_URL = config.get('VIEW_URL', 'http://figshare.com/')
+BASE_URL = config.get('BASE_URL', 'https://api.figshare.com/v2')
+VIEW_URL = config.get('VIEW_URL', 'https://figshare.com/')
+DOWNLOAD_URL = config.get('VIEW_URL', 'https://ndownloader.figshare.com/')
+
+FOLDER_TYPES = [4]
+
+PRIVATE_IDENTIFIER = 'https://api.figshare.com/v2/account/'
+
+# During initial testing this was set to 2 because file was not instantly ready after receiving HTTP 201
+FILE_CREATE_WAIT = 0.1   # seconds passed to time.sleep
