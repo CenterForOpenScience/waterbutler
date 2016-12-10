d9cb66f189d0a47e5093c739b538039ecafd31c3
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2016-12-10 02:38:37 +0000

    Many fixes: folder deletion, tests, errors, GLPath logic
     * adds ability to delete folder with tests
    
     * move logic into GLPath
    
     * improve error handling
    
     * update tests for v3 api

--- tests/providers/gitlab/test_provider.py
+++ tests/providers/gitlab/test_provider.py
@@ -10,6 +10,7 @@ from waterbutler.providers.gitlab import settings as gitlab_settings
 from waterbutler.providers.gitlab.metadata import GitLabFileContentMetadata
 from waterbutler.providers.gitlab.metadata import GitLabFolderContentMetadata
 
+
 @pytest.fixture
 def auth():
     return {
@@ -17,6 +18,98 @@ def auth():
         'email': 'cat@cat.com',
     }
 
+
+@pytest.fixture
+def gitlab_simple_project_tree():
+    return [
+            {
+                "id": "a1e8f8d745cc87e3a9248358d9352bb7f9a0aeba",
+                "name": "folder1",
+                "type": "tree",
+                "path": "folder1",
+                "mode": "040000"
+            },
+            {
+                "id":"a1e8f8d745cc87e3a9248358d9352bb7f9a0aeba",
+                "name": "file1",
+                "type": "blob",
+                "path": "folder1/file1",
+                "mode": "040000"
+            }
+        ]
+
+@pytest.fixture
+def gitlab_example_sub_project_tree():
+    return [
+            {
+                "id": "a1e8f8d745cc87e3a9248358d9352bb7f9a0aeba",
+                "name": ".gitkeep",
+                "type": "blob",
+                "path": "files/html/.gitkeep",
+                "mode": "040000"
+            },
+            {
+                "id": "a1e8f8d745cc87e3a9248358d9352bb7f9a0aeba",
+                "name": ".gitkeep",
+                "type": "tree",
+                "path": "files/html/static",
+                "mode": "040000"
+            }
+        ]
+
+@pytest.fixture
+def gitlab_example_project_tree():
+    return [
+            {
+                "id": "a1e8f8d745cc87e3a9248358d9352bb7f9a0aeba",
+                "name": "html",
+                "type": "tree",
+                "path": "files/html",
+                "mode": "040000"
+            },
+            {
+                "id": "4535904260b1082e14f867f7a24fd8c21495bde3",
+                "name": "images",
+                "type": "tree",
+                "path": "files/images",
+                "mode": "040000"
+            },
+            {
+                "id": "31405c5ddef582c5a9b7a85230413ff90e2fe720",
+                "name": "js",
+                "type": "tree",
+                "path": "files/js",
+                "mode": "040000"
+            },
+            {
+                "id": "cc71111cfad871212dc99572599a568bfe1e7e00",
+                "name": "lfs",
+                "type": "tree",
+                "path": "files/lfs",
+                "mode": "040000"
+            },
+            {
+                "id": "fd581c619bf59cfdfa9c8282377bb09c2f897520",
+                "name": "markdown",
+                "type": "tree",
+                "path": "files/markdown",
+                "mode": "040000"
+            },
+            {
+                "id": "23ea4d11a4bdd960ee5320c5cb65b5b3fdbc60db",
+                "name": "ruby",
+                "type": "tree",
+                "path": "files/ruby",
+                "mode": "040000"
+            },
+            {
+                "id": "7d70e02340bac451f281cecf0a980907974bd8be",
+                "name": "whitespace",
+                "type": "blob",
+                "path": "files/whitespace",
+                "mode": "100644"
+            }
+        ]
 
 @pytest.fixture
 def credentials():
@@ -29,8 +122,7 @@ def settings():
         'owner': 'cat',
         'repo': 'food',
         'repo_id': '123',
-        'base_url': 'http://base.url',
-        'view_url': 'http://view.url',
+        'host': 'http://base.url',
     }
 
 
@@ -57,7 +149,7 @@ def other(auth, credentials, settings, repo_metadata):
 
 class TestHelpers:
     def test_build_repo_url(self, provider, settings):
-        expected = 'http://base.url/projects/123/contents'
+        expected = 'http://base.url/api/v3/projects/123/contents'
         assert provider.build_repo_url('contents') == expected
 
 class TestMetadata:
@@ -68,7 +160,7 @@ class TestMetadata:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/files?ref=master&file_path=folder1/folder2/file'
+        url = 'http://base.url/api/v3/projects/123/repository/files?ref=master&file_path=folder1/folder2/file'
 
         aiohttpretty.register_json_uri('GET', url, body={
                 'file_name': 'file',
@@ -91,7 +183,7 @@ class TestMetadata:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/files?ref=my-branch&file_path=folder1/folder2/file'
+        url = 'http://base.url/api/v3/projects/123/repository/files?ref=my-branch&file_path=folder1/folder2/file'
 
         aiohttpretty.register_json_uri('GET', url, body={
                 'file_name': 'file',
@@ -114,7 +206,7 @@ class TestMetadata:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/tree?path=folder1/folder2/folder3/'
+        url = 'http://base.url/api/v3/projects/123/repository/tree?path=folder1/folder2/folder3/'
 
         aiohttpretty.register_json_uri('GET', url, body=[
             {
@@ -166,7 +258,7 @@ class TestMetadata:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/tree?path=folder1/folder2/folder3/'
+        url = 'http://base.url/api/v3/projects/123/repository/tree?path=folder1/folder2/folder3/'
 
         aiohttpretty.register_json_uri('GET', url, body={})
 
@@ -182,9 +274,61 @@ class TestDelete:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/files?commit_message=File+folder1/file.py+deleted&branch_name=master&file_path=folder1/file.py'
+        url = 'http://base.url/api/v3/projects/123/repository/files?commit_message=File+folder1/file.py+deleted&branch_name=master&file_path=folder1/file.py'
 
         aiohttpretty.register_json_uri('DELETE', url)
+
+        result = await provider.delete(waterbutler_path, branch='master')
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_delete_folder(self, provider):
+
+        path = '/'
+
+        waterbutler_path = await provider.validate_path(path)
+
+        info_url = 'http://base.url/api/v3/projects/123/repository/tree?path=/&ref_name=master'
+        sub_url = 'http://base.url/api/v3/projects/123/repository/tree?path=folder1/&ref_name=master'
+
+        url = 'http://base.url/api/v3/projects/123/repository/files?commit_message=Folder+/+deleted&file_path=folder1/file1&branch_name=master'
+
+        aiohttpretty.register_json_uri('GET', info_url, body=gitlab_simple_project_tree())
+        aiohttpretty.register_json_uri('GET', sub_url, body={})
+        aiohttpretty.register_json_uri('DELETE', url)
+
+        result = await provider.delete(waterbutler_path, branch='master')
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_delete_complete_folder(self, provider):
+
+        path = '/folder3/'
+
+        waterbutler_path = await provider.validate_path(path)
+
+        info_url = 'http://base.url/api/v3/projects/123/repository/tree?path=folder3/&ref_name=master'
+        sub1_url = 'http://base.url/api/v3/projects/123/repository/tree?path=files/html/&ref_name=master'
+        sub2_url = 'http://base.url/api/v3/projects/123/repository/tree?path=files/images/&ref_name=master'
+        sub3_url = 'http://base.url/api/v3/projects/123/repository/tree?path=files/js/&ref_name=master'
+        sub4_url = 'http://base.url/api/v3/projects/123/repository/tree?path=files/lfs/&ref_name=master'
+        sub5_url = 'http://base.url/api/v3/projects/123/repository/tree?path=files/markdown/&ref_name=master'
+        sub6_url = 'http://base.url/api/v3/projects/123/repository/tree?path=files/ruby/&ref_name=master'
+        sub7_url = 'http://base.url/api/v3/projects/123/repository/tree?path=files/html/static/&ref_name=master'
+
+        file1_url = 'http://base.url/api/v3/projects/123/repository/files?file_path=files/whitespace&branch_name=master&commit_message=Folder+folder3/+deleted'
+        file2_url = 'http://base.url/api/v3/projects/123/repository/files?file_path=files/html/.gitkeep&branch_name=master&commit_message=Folder+folder3/+deleted'
+
+        aiohttpretty.register_json_uri('GET', info_url, body=gitlab_example_project_tree())
+        aiohttpretty.register_json_uri('GET', sub1_url, body=gitlab_example_sub_project_tree())
+        aiohttpretty.register_json_uri('GET', sub2_url, body={})
+        aiohttpretty.register_json_uri('GET', sub3_url, body={})
+        aiohttpretty.register_json_uri('GET', sub4_url, body={})
+        aiohttpretty.register_json_uri('GET', sub5_url, body={})
+        aiohttpretty.register_json_uri('GET', sub6_url, body={})
+        aiohttpretty.register_json_uri('GET', sub7_url, body={})
+        aiohttpretty.register_json_uri('DELETE', file1_url)
+        aiohttpretty.register_json_uri('DELETE', file2_url)
 
         result = await provider.delete(waterbutler_path, branch='master')
 
@@ -195,7 +339,7 @@ class TestDelete:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/files?commit_message=custom&branch_name=master&file_path=folder1/file.py'
+        url = 'http://base.url/api/v3/projects/123/repository/files?commit_message=custom&branch_name=master&file_path=folder1/file.py'
 
         aiohttpretty.register_json_uri('DELETE', url)
 
@@ -211,7 +355,7 @@ class TestDownload:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/2123/repository/files?commit_message=File+folder1/file.py+deleted&branch_name=master&file_path=folder1/file.py'
+        url = 'http://base.url/api/v3/projects/2123/repository/files?commit_message=File+folder1/file.py+deleted&branch_name=master&file_path=folder1/file.py'
 
         aiohttpretty.register_json_uri('GET', url)
 
@@ -225,7 +369,7 @@ class TestDownload:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/files?ref=master&file_path=folder1/file.py'
+        url = 'http://base.url/api/v3/projects/123/repository/files?ref=master&file_path=folder1/file.py'
 
         aiohttpretty.register_json_uri('GET', url, body={
             'content': 'aGVsbG8='
@@ -244,13 +388,13 @@ class TestUpload:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/files'
+        url = 'http://base.url/api/v3/projects/123/repository/files'
         aiohttpretty.register_json_uri('POST', url)
 
-        url_put = 'http://base.url/projects/123/repository/files'
+        url_put = 'http://base.url/api/v3/projects/123/repository/files'
         aiohttpretty.register_json_uri('PUT', url)
 
-        url_metadata = 'http://base.url/projects/123/repository/files?file_path=folder1/file.py&ref=master'
+        url_metadata = 'http://base.url/api/v3/projects/123/repository/files?file_path=folder1/file.py&ref=master'
         aiohttpretty.register_json_uri('GET', url_metadata, body={
             'file_name': 'file.py',
             'file_path': path,
@@ -285,10 +429,10 @@ class TestCreateFolter:
 
         waterbutler_path = WaterButlerPath(path)
 
-        url = 'http://base.url/projects/123/repository/files'
+        url = 'http://base.url/api/v3/projects/123/repository/files'
         aiohttpretty.register_json_uri('POST', url)
 
-        url_get_metadata = 'http://base.url/projects/123/repository/files?file_path=folder1/.gitkeep&ref=master'
+        url_get_metadata = 'http://base.url/api/v3/projects/123/repository/files?file_path=folder1/.gitkeep&ref=master'
         aiohttpretty.register_json_uri('GET', url_get_metadata, body={
             'file_name': '.gitkeep',
             'file_path': '/folder1/.gitkeep',
@@ -297,7 +441,7 @@ class TestCreateFolter:
             'commit_id': '1442422sss',
         })
 
-        url_put = 'http://base.url/projects/123/repository/files'
+        url_put = 'http://base.url/api/v3/projects/123/repository/files'
         aiohttpretty.register_json_uri('PUT', url)
 
         result = await provider.create_folder(waterbutler_path, 'master', 'commit message')
@@ -317,49 +461,114 @@ class TestValidatePath:
     @pytest.mark.aiohttpretty
     async def test_validate_path_v1_is_file(self, provider):
         path = '/folder1/file.py'
-        url_get = 'http://base.url/projects/123'
-        aiohttpretty.register_json_uri('GET', url_get, body={'default_branch': 'master'})
+        url = 'http://base.url/api/v3/projects/123/repository/tree?path=folder1/file.py'
+        aiohttpretty.register_json_uri('GET', url, body={})
 
-        validated_path = await provider.validate_v1_path(path)
+        validated = await provider.validate_v1_path(path)
 
-        assert validated_path.is_file
-        assert not validated_path.is_dir
+        assert validated.is_file
 
     @pytest.mark.asyncio
     @pytest.mark.aiohttpretty
     async def test_validate_path_v1_is_dir(self, provider):
-        path = '/folder1/folder2/'
-        url_get = 'http://base.url/projects/123'
-        aiohttpretty.register_json_uri('GET', url_get, body={'default_branch': 'master'})
+        path = '/folder1/'
+        body = gitlab_simple_project_tree()
+        url = 'http://base.url/api/v3/projects/123/repository/tree?path=folder1/'
+        aiohttpretty.register_json_uri('GET', url, body=body)
 
-        validated_path = await provider.validate_v1_path(path)
+        validated = await provider.validate_v1_path(path)
 
-        assert validated_path.is_dir
-        assert not validated_path.is_file
+        assert validated.is_dir
 
     @pytest.mark.asyncio
     @pytest.mark.aiohttpretty
-    async def test_validate_path_v1_with_wrong_http_response(self, provider):
-        path = '/folder1/file.py'
-        url_get = 'http://base.url/projects/123'
-        aiohttpretty.register_json_uri('GET', url_get, status=400)
+    async def test_validate_v1_root(self, provider):
+        path = '/'
+        url_get = 'http://base.url/api/v3/projects/123/repository/tree?path=/'
+        body = gitlab_simple_project_tree()
+        aiohttpretty.register_json_uri('GET', url_get, body=body, status=200)
 
-        with pytest.raises(exceptions.NotFoundError) as exc:
-            validated_path = await provider.validate_v1_path(path)
+        validated = await provider.validate_v1_path(path)
+
+        assert validated.is_dir
+        assert validated.is_root
+
+    @pytest.mark.asyncio
+    async def test_validate_file_path_from_gitlab(self, provider):
+        path = 'folder1/file.py'
+
+        validated = await provider.validate_v1_path(path)
+
+        assert validated.is_file
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_validate_folder_path_from_gitlab(self, provider):
+        path = 'folder1/'
+        url_get = 'http://base.url/api/v3/projects/123/repository/tree?path=folder1/'
+        body = gitlab_simple_project_tree()
+        aiohttpretty.register_json_uri('GET', url_get, body=body, status=200)
+
+        validated = await provider.validate_v1_path(path)
+
+        assert validated.is_dir
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_validate_folder_composed_path_from_gitlab(self, provider):
+        path = 'files/html/'
+        url_get = 'http://base.url/api/v3/projects/123/repository/tree?path=files/html/'
+        body = gitlab_example_project_tree()
+        aiohttpretty.register_json_uri('GET', url_get, body=body, status=200)
+
+        validated = await provider.validate_v1_path(path)
+
+        assert validated.is_dir
 
     @pytest.mark.asyncio
     async def test_validate_path_is_file(self, provider):
         path = '/folder1/file.py'
 
-        validated_path = await provider.validate_path(path)
+        validated = await provider.validate_path(path)
 
-        assert validated_path.is_file
-        assert not validated_path.is_dir
+        assert validated.is_file
 
     @pytest.mark.asyncio
     async def test_validate_path_is_dir(self, provider):
         path = '/folder1/folder2/'
-        validated_path = await provider.validate_path(path)
+        validated = await provider.validate_path(path)
 
-        assert validated_path.is_dir
-        assert not validated_path.is_file
+        assert validated.is_dir
+
+    @pytest.mark.asyncio
+    async def test_validate_file_path_from_gitlab(self, provider):
+        path = 'folder1/file.py'
+
+        validated = await provider.validate_path(path)
+
+        assert validated.is_file
+
+    @pytest.mark.asyncio
+    async def test_validate_folder_path_from_gitlab(self, provider):
+        path = 'folder1/'
+
+        validated = await provider.validate_path(path)
+
+        assert validated.is_dir
+
+    @pytest.mark.asyncio
+    async def test_validate_folder_composed_path_from_gitlab(self, provider):
+        path = 'folder1/folder2/'
+
+        validated = await provider.validate_path(path)
+
+        assert validated.is_dir
+
+    @pytest.mark.asyncio
+    async def test_validate_root(self, provider):
+        path = '/'
+
+        validated = await provider.validate_path(path)
+
+        assert validated.is_dir
+        assert validated.is_root
