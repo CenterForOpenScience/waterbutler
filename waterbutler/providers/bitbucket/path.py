ff4900dbd221f178414fe820ed467cf36a3e914f
Fitz Elliott <fitz@cos.io>
2017-03-07 10:19:58 -0500

    add new read-only bitbucket provider

---
+++ waterbutler/providers/bitbucket/path.py
@@ -0,0 +1,48 @@
+from waterbutler.core import path
+
+
+class BitbucketPathPart(path.WaterButlerPathPart):
+    def increment_name(self, _id=None):
+        """Overridden to preserve branch from _id upon incrementing"""
+        self._id = _id or (self._id[0], self._id[1])
+        self._count += 1
+        return self
+
+
+class BitbucketPath(path.WaterButlerPath):
+    """The ``identifier`` for BitbucketPaths are tuples of ``(commit_sha, branch_name)``. Children
+    of BitbucketPaths inherit their parent's ``commit_sha`` and ``branch_name``.  Either one may be
+    ``None``.
+    """
+    PART_CLASS = BitbucketPathPart
+
+    @property
+    def commit_sha(self):
+        return self.identifier[0]
+
+    @property
+    def branch_name(self):
+        """Name of branch this path is on.  May be ``None`` if commit sha was given."""
+        return self.identifier[1]
+
+    @property
+    def ref(self):
+        """commit sha or branch name on which this file exists"""
+        return self.commit_sha or self.branch_name
+
+    @property
+    def extra(self):
+        return dict(super().extra, **{
+            'commitSha': self.commit_sha,
+            'branchName': self.branch_name,
+        })
+
+    def child(self, name, _id=None, folder=False):
+        """Pass current branch down to children"""
+        if _id is None:
+            _id = (self.commit_sha, self.branch_name)
+        return super().child(name, _id=_id, folder=folder)
+
+    def set_commit_sha(self, commit_sha):
+        for part in self.parts:
+            part._id = (commit_sha, part._id[1])
