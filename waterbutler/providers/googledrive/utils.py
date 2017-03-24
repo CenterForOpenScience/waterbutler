91ee5d82c0398a5474c8ed9237a1827cbb120049
John Tordoff <john@cos.io>
2017-03-24 15:06:48 -0400

    Disambiguate between files returned from API made to GDrive without extensions, also WB will now throw error when attempting to download a file when the user only has ‘veiw_only’ permissions for the GFile.

--- waterbutler/providers/googledrive/utils.py
+++ waterbutler/providers/googledrive/utils.py
@@ -1,3 +1,5 @@
+from waterbutler.core import exceptions
+
 DOCS_FORMATS = [
     {
         'mime_type': 'application/vnd.google-apps.document',
@@ -54,6 +56,16 @@ def get_extension(metadata):
     return format['ext']
 
 
+def get_ext_from_mime_type(mime_type):
+    for format in DOCS_FORMATS:
+        if format['mime_type'] == mime_type:
+            return format['ext']
+
+
+def ext_is_gfile(ext):
+    return ext in [x['ext'] for x in DOCS_FORMATS]
+
+
 def get_download_extension(metadata):
     format = get_format(metadata)
     return format['download_ext']
@@ -61,4 +73,31 @@ def get_download_extension(metadata):
 
 def get_export_link(metadata):
     format = get_format(metadata)
-    return metadata['exportLinks'][format['type']]
+    if metadata.get('exportLinks'):
+        return metadata['exportLinks'].get(format['type'])
+
+
+def disambiguate_files(ext, resp_items, path):
+    """
+    When calling for an extension-less file there are two cases:
+    1. It's a random file the user uploaded without an extension
+    2. It's a GFile that's hiding it's extension
+
+    We want the random file when possible, but we also have to disambiguate between GFiles and we
+    do that using mime type.
+
+    :param ext: The extension we are given as part of the path, for GFiles it should always be given.
+    :param resp_items: The json dicts returned from a GDrive query
+    :return: The item the user was looking for.
+    """
+
+    if ext_is_gfile(ext):
+        items = [x for x in resp_items if x['mimeType'] == get_mimetype_from_ext(ext)]
+    else:
+        # GFiles never include a 'fileExtension' field, but extension-less files have them as ''
+        items = [x for x in resp_items if x.get('fileExtension') == '']
+
+    if len(items) == 1:
+        return items[0]
+    else:
+        raise exceptions.NotFoundError(path)
