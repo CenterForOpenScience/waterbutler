d70ae0012d89422d2c87672be694ad8963438d17
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2017-04-10 15:08:20 -0300

    Make this provider read-only / support default branch

--- waterbutler/providers/gitlab/exceptions.py
+++
@@ -1,9 +0,0 @@
-from waterbutler.core.exceptions import ProviderError
-
-
-class GitLabUnsupportedRepoError(ProviderError):
-    def __init__(self):
-        message = ('Some folder operations on large GitLab repositories cannot be supported without'
-                   ' data loss.  To carry out this operation, please perform it in a local git'
-                   ' repository, then push to the target repository on GitLab.')
-        super().__init__(message, code=501)
