69d457f1baae95f59aed2b47cee1d8e65e0e3192
Fitz Elliott <fitz@cos.io>
2017-07-31 12:21:02 -0400

    remove unused and obsolete code

--- waterbutler/providers/gitlab/settings.py
+++ waterbutler/providers/gitlab/settings.py
@@ -1,11 +1,3 @@
 from waterbutler import settings
 
 config = settings.child('GITLAB_PROVIDER_CONFIG')
-
-
-MOVE_MESSAGE = config.get('MOVE_MESSAGE', 'Moved on behalf of WaterButler')
-COPY_MESSAGE = config.get('COPY_MESSAGE', 'Copied on behalf of WaterButler')
-DELETE_FILE_MESSAGE = config.get('DELETE_FILE_MESSAGE', 'File deleted on behalf of WaterButler')
-UPDATE_FILE_MESSAGE = config.get('UPDATE_FILE_MESSAGE', 'File updated on behalf of WaterButler')
-UPLOAD_FILE_MESSAGE = config.get('UPLOAD_FILE_MESSAGE', 'File uploaded on behalf of WaterButler')
-DELETE_FOLDER_MESSAGE = config.get('DELETE_FOLDER_MESSAGE', 'Folder deleted on behalf of WaterButler')
