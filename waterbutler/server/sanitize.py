264e48198bc8e6a1e510daadc2ee5352bb0cc232
TomBaxter <tkb608@gmail.com>
2017-12-27 09:09:26 -0500

    Refactor custom raven processor
    SVCS-334
    
    Refactor subclass of SanitizePasswordsProcessor to take better advantage of inheritance.

--- waterbutler/server/sanitize.py
+++ waterbutler/server/sanitize.py
@@ -4,43 +4,26 @@ from raven.processors import SanitizePasswordsProcessor
 
 
 class WBSanitizer(SanitizePasswordsProcessor):
-    """Asterisk out things that look like passwords, keys, etc."""
+    """
+    Use parent class to asterisk out things that look like passwords, credit card numbers,
+    and API keys in frames, http, and basic extra data.
 
-    # Store mask as a fixed length for security
-    MASK = '*' * 8
-
-    # Token and key added from original. Key is used by Dataverse
-    FIELDS = frozenset([
-        'password',
-        'secret',
-        'passwd',
-        'authorization',
-        'api_key',
-        'apikey',
-        'sentry_dsn',
-        'access_token',
-        'key',
-        'token',
-    ])
-
-    # Credit card regex left intact from original processor
-    # While we should never have credit card information, its still best to perform the check
-    # and keep old functionality
-    VALUES_RE = re.compile(r'^(?:\d[ -]*?){13,16}$')
+    In addition, asterisk out Dataverse formatted ouath tokens.
+    """
 
     # Should specifically match Dataverse secrets. Key format checked on demo and on Harvard
     DATAVERSE_SECRET_RE = re.compile(r'[A-Za-z0-9]{8}-[A-Za-z0-9]{4}-[A-Za-z0-9]'
                                                 '{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12}')
 
+    def __init__(self):
+        # As of raven version 6.4 this attribute name has been changed from FIELDS to KEYS.
+        # Will need to be updated when we upgrade.
+        self.FIELDS = self.FIELDS.union(['key', 'token', 'refresh_token'])
+
     def sanitize(self, key, value):
-        """Overload the sanitize function of the `SanitizePasswordsProcessor'."""
+        """Subclass the sanitize function of the `SanitizePasswordsProcessor'."""
 
-        if value is None:
-            return
-
-        # Part of the original method. Looks for credit cards to sanitize
-        if isinstance(value, str) and self.VALUES_RE.search(value):
-            return self.MASK
+        value = SanitizePasswordsProcessor.sanitize(self, key, value)
 
         if isinstance(value, dict):
             for item in value:
@@ -50,7 +33,6 @@ class WBSanitizer(SanitizePasswordsProcessor):
             new_list = []
             for item in value:
                 new_list.append(self.sanitize(key, item))
-
             value = new_list
 
         # Check for Dataverse secrets
@@ -58,23 +40,5 @@ class WBSanitizer(SanitizePasswordsProcessor):
             matches = self.DATAVERSE_SECRET_RE.findall(value)
             for match in matches:
                 value = value.replace(match, self.MASK)
-
-        # key can be a NoneType
-        # This sould be after the regex checks incase a `None` key is a token
-        if not key:
-            return value
-
-        # Just in case we have bytes here, we want to turn them into text
-        # properly without failing so we can perform our check.
-        if isinstance(key, bytes):
-            # May want a try/except block around this, but for now it should be okay
-            key = key.decode('utf-8', 'replace')
-        else:
-            key = str(key)
-
-        key = key.lower()
-        for field in self.FIELDS:
-            if field in key:
-                return self.MASK
 
         return value
