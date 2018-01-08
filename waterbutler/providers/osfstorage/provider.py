3460c54e7f14dbe470acb254a4a65820ef338beb
Fitz Elliott <fitz@cos.io>
2018-01-08 15:12:08 -0500

    redact information from user-visible error message
     * If an UploadFailedError occurs, the user will see the constructed
       error message.  It's unlikely they'll be able to fix the error with
       the underlying exceptions, so just ask them to try uploading again.
       The exceptions may also contain information that we don't want to
       leak publicly.  The actual exceptions will still be available to
       developers in Sentry and in the terminal logs.

--- waterbutler/providers/osfstorage/provider.py
+++ waterbutler/providers/osfstorage/provider.py
@@ -327,9 +327,8 @@ class OSFStorageProvider(provider.BaseProvider):
             try:
                 os.remove(local_pending_path)
             except OSError as os_exc:
-                raise exceptions.UploadFailedError('Upload failed and attempts to clean pending '
-                                                   'files failed:\n{}\n{}'.format(exc, os_exc))
-            raise exceptions.UploadError('Upload failed and pending files cleaned:\n{}'.format(exc))
+                raise exceptions.UploadFailedError('Upload failed, please try again.') from os_exc
+            raise exceptions.UploadFailedError('Upload failed, please try again.') from exc
 
         complete_name = stream.writers['sha256'].hexdigest
         local_complete_path = os.path.join(settings.FILE_PATH_COMPLETE, complete_name)
