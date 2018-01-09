4864c2de7b100f676ad76b306c7120c20ade048d
John Tordoff <john@cos.io>
2018-01-09 16:32:32 -0500

    remove erroneous comment

--- waterbutler/server/api/v1/provider/__init__.py
+++ waterbutler/server/api/v1/provider/__init__.py
@@ -26,7 +26,6 @@ def list_or_value(value):
     if len(value) == 0:
         return None
     if len(value) == 1:
-        # Remove leading slashes as they break things
         return value[0].decode('utf-8')
     return [item.decode('utf-8') for item in value]
 
