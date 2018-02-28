4a2c6fa3caebe89041af13871f7538a0d46f3f78
satoshi-yazawa-nii <satoshi-yazawa@nii.ac.jp>
2018-03-01 08:08:17 +0900

    Fix default path of draft directory

--- waterbutler/providers/weko/settings.py
+++ waterbutler/providers/weko/settings.py
@@ -2,4 +2,4 @@ from waterbutler import settings
 
 config = settings.child('WEKO_PROVIDER_CONFIG')
 
-FILE_PATH_DRAFT = config.get('FILE_PATH_DRAFT', '/code/weko-draft/')
+FILE_PATH_DRAFT = config.get('FILE_PATH_DRAFT', '/code/website/wekodraft/')
