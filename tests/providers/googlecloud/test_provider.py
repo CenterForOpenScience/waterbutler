c270be022ac34387bbc757fbe37b4ad2c741b110
longze chen <cs.longze.chen@gmail.com>
2018-03-13 14:15:38 -0400

    Futher improve tests for GoogleCloud
    - Removed deprecated `fixture.py`, which is now
      replaced by `providers.py`, `folders.py` and
      `files.py` in the `fixtures/` directory.
    - Updated fixutres for CRUD operations and added
      back CRUD tests that were accidentally removed
    - Fix exipration check in utility tests where it
      now use the `settings.SIGNATURE_EXPIRATION` to
      calculate the expected exipiration time.
    
    Remove redundant type casting for expiration
    
    - Type casting is now done in the settings.py. I
      assume that this piece of code is accidentally
      left here during the last merge.

--- tests/providers/googlecloud/test_provider.py
+++ tests/providers/googlecloud/test_provider.py
@@ -8,16 +8,11 @@ from aiohttp import MultiDict
 import pytest
 import aiohttpretty
 
-from waterbutler.core.path import WaterButlerPath
-from waterbutler.core.streams import FileStreamReader, ResponseStreamReader
-from waterbutler.core import exceptions as core_exceptions
 from waterbutler.providers.googlecloud import utils
+from waterbutler.core import exceptions as core_exceptions
 from waterbutler.providers.googlecloud import settings as pd_settings
-from waterbutler.providers.googlecloud import (GoogleCloudProvider,
-                                               BaseGoogleCloudMetadata,
-                                               GoogleCloudFileMetadata,
-                                               GoogleCloudFolderMetadata,
-                                               )
+from waterbutler.core.streams import FileStreamReader, ResponseStreamReader
+from waterbutler.providers.googlecloud import GoogleCloudProvider, GoogleCloudFileMetadata
 
 from tests.providers.googlecloud.fixtures.providers import (mock_auth,
                                                             mock_auth_2,
@@ -28,10 +23,14 @@ from tests.providers.googlecloud.fixtures.providers import (mock_auth,
                                                             )
 
 from tests.providers.googlecloud.fixtures.files import (file_raw,
+                                                        file_name,
                                                         file_wb_path,
                                                         file_obj_name,
                                                         meta_file_raw,
                                                         meta_file_parsed,
+                                                        meta_file_upload_raw,
+                                                        meta_file_copy_raw,
+                                                        file_2_wb_path,
                                                         file_2_obj_name,
                                                         file_2_copy_obj_name,
                                                         )
@@ -56,13 +55,11 @@ def mock_time(monkeypatch):
 
 
 @pytest.fixture
-def file_stream_file_1(file_raw):
+def file_stream_file(file_raw):
     return FileStreamReader(io.BytesIO(file_raw))
 
 
 class TestProviderInit:
-    """Test that provider initialization set properties correctly.
-    """
 
     async def test_provider_init(self, mock_provider):
 
@@ -124,13 +121,13 @@ class TestMetadata:
         file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
         signed_url = mock_provider._build_and_sign_url('HEAD', file_obj_name, **{})
 
+        # Must use `MultiDict` and modify the headers to mock the `aiohttp`'s response headers
         resp_headers = MultiDict(json.loads(meta_file_raw))
         google_hash = resp_headers.get('x-goog-hash', None)
         resp_headers.pop('x-goog-hash')
         google_hash_list = google_hash.split(',')
         for google_hash in google_hash_list:
             resp_headers.add('x-goog-hash', google_hash)
-        metadata_json = json.loads(meta_file_parsed)
 
         aiohttpretty.register_uri(
             'HEAD',
@@ -139,19 +136,21 @@ class TestMetadata:
             status=HTTPStatus.OK
         )
 
-        metadata = await mock_provider._metadata_object(file_wb_path, is_folder=False)
+        metadata_json = json.loads(meta_file_parsed)
         metadata_expected = GoogleCloudFileMetadata(metadata_json)
+
+        metadata = await mock_provider._metadata_object(file_wb_path, is_folder=False)
+
         assert isinstance(metadata, GoogleCloudFileMetadata)
         assert metadata == metadata_expected
-
 
     @pytest.mark.asyncio
     @pytest.mark.aiohttpretty
     async def test_metadata_object_401_unauthorized(
             self,
             mock_time,
-            file_wb_path,
-            mock_provider
+            mock_provider,
+            file_wb_path
     ):
         file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
         signed_url = mock_provider._build_and_sign_url('HEAD', file_obj_name, **{})
@@ -166,14 +165,15 @@ class TestMetadata:
             await mock_provider._metadata_object(file_wb_path, is_folder=False)
 
         assert exc.value.code == HTTPStatus.UNAUTHORIZED
+        assert aiohttpretty.has_call(method='HEAD', uri=signed_url)
 
     @pytest.mark.asyncio
     @pytest.mark.aiohttpretty
     async def test_metadata_object_404_not_found(
             self,
             mock_time,
-            file_wb_path,
             mock_provider,
+            file_wb_path
     ):
         file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
         signed_url = mock_provider._build_and_sign_url('HEAD', file_obj_name, **{})
@@ -188,6 +188,7 @@ class TestMetadata:
             await mock_provider._metadata_object(file_wb_path, is_folder=False)
 
         assert exc.value.code == HTTPStatus.NOT_FOUND
+        assert aiohttpretty.has_call(method='HEAD', uri=signed_url)
 
 
 class TestOperations:
@@ -214,3 +215,302 @@ class TestOperations:
     def test_can_duplicate_names(self, mock_provider):
 
         assert mock_provider.can_duplicate_names()
+
+
+class TestCRUD:
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_download_file(
+            self,
+            mock_time,
+            mock_provider,
+            file_wb_path,
+            file_raw
+    ):
+        file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
+        signed_url = mock_provider._build_and_sign_url('GET', file_obj_name, **{})
+
+        aiohttpretty.register_uri(
+            'GET',
+            signed_url,
+            body=file_raw,
+            status=HTTPStatus.OK
+        )
+
+        resp_stream_reader = await mock_provider.download(file_wb_path)
+        file_content = await resp_stream_reader.read()
+
+        assert aiohttpretty.has_call(method='GET', uri=signed_url)
+        assert isinstance(resp_stream_reader, ResponseStreamReader)
+        assert file_content == file_raw
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_download_file_with_accept_url(
+            self,
+            mock_time,
+            mock_provider,
+            file_wb_path,
+            file_name
+    ):
+
+        file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
+        query = {'response-content-disposition': 'attachment; filename={}'.format(file_name)}
+        signed_url = mock_provider._build_and_sign_url('GET', file_obj_name, **query)
+        return_url = await mock_provider.download(file_wb_path, accept_url=True, display_name=file_name)
+
+        assert not aiohttpretty.has_call(method='GET', uri=signed_url)
+        assert isinstance(return_url, str)
+        assert signed_url == return_url
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_download_file_not_found(self, mock_time, mock_provider, file_wb_path):
+
+        file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
+        signed_url = mock_provider._build_and_sign_url('GET', file_obj_name, **{})
+
+        aiohttpretty.register_uri(
+            'GET',
+            signed_url,
+            status=HTTPStatus.NOT_FOUND
+        )
+
+        with pytest.raises(core_exceptions.DownloadError) as exc:
+            await mock_provider.download(file_wb_path, is_folder=False)
+
+        assert exc.value.code == HTTPStatus.NOT_FOUND
+        assert aiohttpretty.has_call(method='GET', uri=signed_url)
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_upload_file(
+            self,
+            mock_time,
+            mock_provider,
+            file_wb_path,
+            meta_file_raw,
+            meta_file_parsed,
+            meta_file_upload_raw,
+            file_stream_file
+    ):
+        file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
+
+        signed_url_upload = mock_provider._build_and_sign_url('PUT', file_obj_name, **{})
+        # There is no need to use `MultiDict` since the hashes are not used
+        resp_headers = json.loads(meta_file_upload_raw)
+        aiohttpretty.register_uri(
+            'PUT',
+            signed_url_upload,
+            headers=resp_headers,
+            status=HTTPStatus.OK
+        )
+
+        signed_url_metadata = mock_provider._build_and_sign_url('HEAD', file_obj_name, **{})
+        # Must use `MultiDict` and modify the headers to mock the `aiohttp`'s response headers
+        resp_headers = MultiDict(json.loads(meta_file_raw))
+        google_hash = resp_headers.get('x-goog-hash', None)
+        resp_headers.pop('x-goog-hash')
+        google_hash_list = google_hash.split(',')
+        for google_hash in google_hash_list:
+            resp_headers.add('x-goog-hash', google_hash)
+
+        aiohttpretty.register_uri(
+            'HEAD',
+            signed_url_metadata,
+            headers=resp_headers,
+            status=HTTPStatus.OK
+        )
+
+        metadata_json = json.loads(meta_file_parsed)
+        metadata_expected = GoogleCloudFileMetadata(metadata_json)
+
+        metadata, _ = await mock_provider.upload(file_stream_file, file_wb_path)
+
+        assert metadata == metadata_expected
+        assert aiohttpretty.has_call(method='PUT', uri=signed_url_upload)
+        assert aiohttpretty.has_call(method='HEAD', uri=signed_url_metadata)
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_upload_file_checksum_mismatch(
+            self,
+            mock_time,
+            mock_provider,
+            file_wb_path,
+            meta_file_raw,
+            meta_file_upload_raw,
+            file_stream_file
+    ):
+        file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
+
+        signed_url_upload = mock_provider._build_and_sign_url('PUT', file_obj_name, **{})
+        # There is no need to use `MultiDict` since the hashes are not used
+        resp_headers = json.loads(meta_file_upload_raw)
+        resp_headers.update({'etag': '"9e780e1c4ee28c44642160b349b3aab0"'})
+        aiohttpretty.register_uri(
+            'PUT',
+            signed_url_upload,
+            headers=resp_headers,
+            status=HTTPStatus.OK
+        )
+
+        signed_url_metadata = mock_provider._build_and_sign_url('HEAD', file_obj_name, **{})
+        # There is no need to use `MultiDict` since the hashes are not used
+        resp_headers = MultiDict(json.loads(meta_file_raw))
+        aiohttpretty.register_uri(
+            'HEAD',
+            signed_url_metadata,
+            headers=resp_headers,
+            status=HTTPStatus.OK
+        )
+
+        with pytest.raises(core_exceptions.UploadChecksumMismatchError) as exc:
+            await mock_provider.upload(file_stream_file, file_wb_path)
+
+        assert exc.value.code == HTTPStatus.INTERNAL_SERVER_ERROR
+        assert aiohttpretty.has_call(method='HEAD', uri=signed_url_metadata)
+        assert aiohttpretty.has_call(method='PUT', uri=signed_url_upload)
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_delete_file(self, mock_time, mock_provider, file_wb_path):
+
+        file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
+        signed_url = mock_provider._build_and_sign_url('DELETE', file_obj_name, **{})
+
+        aiohttpretty.register_uri(
+            'DELETE',
+            signed_url,
+            status=HTTPStatus.NO_CONTENT
+        )
+
+        await mock_provider.delete(file_wb_path)
+
+        assert aiohttpretty.has_call(method='DELETE', uri=signed_url)
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_delete_file_not_found(self, mock_time, mock_provider, file_wb_path):
+
+        file_obj_name = utils.get_obj_name(file_wb_path, is_folder=False)
+        signed_url = mock_provider._build_and_sign_url('DELETE', file_obj_name, **{})
+
+        aiohttpretty.register_uri(
+            'DELETE',
+            signed_url,
+            status=HTTPStatus.NOT_FOUND
+        )
+
+        with pytest.raises(core_exceptions.DeleteError) as exc:
+            await mock_provider.delete(file_wb_path)
+
+        assert exc.value.code == HTTPStatus.NOT_FOUND
+        assert aiohttpretty.has_call(method='DELETE', uri=signed_url)
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_intra_copy_file(
+            self,
+            mock_time,
+            mock_provider,
+            file_wb_path,
+            file_2_wb_path,
+            meta_file_raw,
+            meta_file_parsed,
+            meta_file_copy_raw
+    ):
+        src_file_path = file_2_wb_path
+        dest_file_path = file_wb_path
+        src_file_obj_name = utils.get_obj_name(src_file_path, is_folder=False)
+        dest_file_obj_name =utils.get_obj_name(dest_file_path, is_folder=False)
+
+        object_name_with_bucket = '{}/{}'.format(mock_provider.bucket, src_file_obj_name)
+        canonical_ext_headers = {'x-goog-copy-source': object_name_with_bucket}
+        signed_url_intra_copy = mock_provider._build_and_sign_url(
+            'PUT',
+            dest_file_obj_name,
+            canonical_ext_headers=canonical_ext_headers,
+            **{}
+        )
+        resp_headers = json.loads(meta_file_copy_raw)
+        aiohttpretty.register_uri(
+            'PUT',
+            signed_url_intra_copy,
+            headers=resp_headers,
+            status=HTTPStatus.OK
+        )
+
+        signed_url_metadata = mock_provider._build_and_sign_url('HEAD', dest_file_obj_name, **{})
+        # Must use `MultiDict` and modify the headers to mock the `aiohttp`'s response headers
+        resp_headers = MultiDict(json.loads(meta_file_raw))
+        google_hash = resp_headers.get('x-goog-hash', None)
+        resp_headers.pop('x-goog-hash')
+        google_hash_list = google_hash.split(',')
+        for google_hash in google_hash_list:
+            resp_headers.add('x-goog-hash', google_hash)
+        aiohttpretty.register_uri(
+            'HEAD',
+            signed_url_metadata,
+            headers=resp_headers,
+            status=HTTPStatus.OK
+        )
+
+        metadata_json = json.loads(meta_file_parsed)
+        metadata_expected = GoogleCloudFileMetadata(metadata_json)
+
+        metadata, _ = await mock_provider.intra_copy(mock_provider, src_file_path, dest_file_path)
+
+        assert metadata == metadata_expected
+        assert aiohttpretty.has_call(method='PUT', uri=signed_url_intra_copy)
+        assert aiohttpretty.has_call(method='HEAD', uri=signed_url_metadata)
+
+    @pytest.mark.asyncio
+    @pytest.mark.aiohttpretty
+    async def test_intra_copy_file_not_found(
+            self,
+            mock_time,
+            mock_provider,
+            file_wb_path,
+            file_2_wb_path,
+            meta_file_raw,
+            meta_file_copy_raw
+    ):
+        src_file_path = file_2_wb_path
+        dest_file_path = file_wb_path
+        src_file_obj_name = utils.get_obj_name(src_file_path, is_folder=False)
+        dest_file_obj_name =utils.get_obj_name(dest_file_path, is_folder=False)
+
+        object_name_with_bucket = '{}/{}'.format(mock_provider.bucket, src_file_obj_name)
+        canonical_ext_headers = {'x-goog-copy-source': object_name_with_bucket}
+        signed_url_intra_copy = mock_provider._build_and_sign_url(
+            'PUT',
+            dest_file_obj_name,
+            canonical_ext_headers=canonical_ext_headers,
+            **{}
+        )
+        resp_headers = json.loads(meta_file_copy_raw)
+        aiohttpretty.register_uri(
+            'PUT',
+            signed_url_intra_copy,
+            headers=resp_headers,
+            status=HTTPStatus.NOT_FOUND
+        )
+
+        signed_url_metadata = mock_provider._build_and_sign_url('HEAD', dest_file_obj_name, **{})
+        # There is no need to use `MultiDict` since the hashes are not used
+        resp_headers = json.loads(meta_file_raw)
+        aiohttpretty.register_uri(
+            'HEAD',
+            signed_url_metadata,
+            headers=resp_headers,
+            status=HTTPStatus.OK
+        )
+
+        with pytest.raises(core_exceptions.CopyError) as exc:
+            await mock_provider.intra_copy(mock_provider, src_file_path, dest_file_path)
+
+        assert exc.value.code == HTTPStatus.NOT_FOUND
+        assert aiohttpretty.has_call(method='PUT', uri=signed_url_intra_copy)
+        assert aiohttpretty.has_call(method='HEAD', uri=signed_url_metadata)
