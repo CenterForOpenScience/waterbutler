14388eb2429000889e326df1b9316cd5c1f6e498
longze chen <cs.longze.chen@gmail.com>
2018-03-13 09:38:53 -0400

    Explicit parse SIGNATURE_EXPIRATION to integer
    Note: environment variables are passed in as string

--- waterbutler/providers/googlecloud/settings.py
+++ waterbutler/providers/googlecloud/settings.py
@@ -6,4 +6,4 @@ config = settings.child('GOOGLECLOUD_PROVIDER_SETTINGS')
 BASE_URL = config.get('BASE_URL', 'https://storage.googleapis.com')
 
 # The expiration time (in seconds) for a signed request
-SIGNATURE_EXPIRATION = config.get('SIGNATURE_EXPIRATION', 60)
+SIGNATURE_EXPIRATION = int(config.get('SIGNATURE_EXPIRATION', 60))
