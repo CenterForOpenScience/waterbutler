9c3b03d9a7e1aeb5a4c7748bc51723403bf3f24c
longze chen <cs.longze.chen@gmail.com>
2018-04-03 16:09:25 -0400

    Improve how WB handles GitHub Rate-limiting (GH-RL)
     - Use resp header to detect exceeding rate limits instead of str
       comparison on resp message
    
     - Rename variables and methods for better clarity
    
     - Add in-depth comments/quirks on how GH-RL works and how we handle
       it, including limitations
    
     - Move GH-RL configs into settings
    
     - Improve control statements
    
     - Update provider exceptions
    
     - Fix mypy for github exceptions. The return type of "__init__" must
       be None
    
     - Update tests for provider and exceptions

--- waterbutler/providers/github/exceptions.py
+++ waterbutler/providers/github/exceptions.py
@@ -5,11 +5,13 @@ from waterbutler.core.exceptions import ProviderError
 
 
 class GitHubUnsupportedRepoError(ProviderError):
-    def __init__(self, dummy):
+
+    def __init__(self, dummy) -> None:
         """``dummy`` argument is because children of ``WaterButlerError`` must be instantiable with
-        a single integer argument.  See :class:`waterbutler.core.exceptions.WaterButlerError`
-        for details.
+        a single integer argument.  See :class:`waterbutler.core.exceptions.WaterButlerError` for
+        details.
         """
+
         super().__init__('Some folder operations on large GitHub repositories cannot be supported '
                          'without data loss.  To carry out this operation, please perform it in a '
                          'local git repository, then push to the target repository on GitHub.',
@@ -17,9 +19,13 @@ class GitHubUnsupportedRepoError(ProviderError):
 
 
 class GitHubRateLimitExceededError(ProviderError):
-    def __init__(self, retry: int):
+
+    def __init__(self, retry: int) -> None:
+
+        # TODO: should we add more information to this exception, e.g. which OSF user or GH account?
         retry_date = time.gmtime(retry)
-        iso_date = '{}-{}-{}T{}:{}:{}+00:00'.format(retry_date.tm_year, retry_date.tm_mon,
-                                                    retry_date.tm_mday, retry_date.tm_hour,
-                                                    retry_date.tm_min, retry_date.tm_sec)
-        super().__init__('Rate limit exceeded. New quota will be available after {}.'.format(iso_date), code=HTTPStatus.SERVICE_UNAVAILABLE, is_user_error=True)
+        iso_date = time.strftime('%Y-%m-%dT%H:%M:%S+00:00', retry_date)
+
+        super().__init__('WB has exceeded GitHub\'s rate limit for this user.  New quota will be '
+                         'available after the limit gets reset at {}.'.format(iso_date),
+                         code=HTTPStatus.SERVICE_UNAVAILABLE, is_user_error=True)
--- waterbutler/providers/github/settings.py
+++ waterbutler/providers/github/settings.py
@@ -13,7 +13,6 @@ UPDATE_FILE_MESSAGE = config.get('UPDATE_FILE_MESSAGE', 'File updated on behalf 
 UPLOAD_FILE_MESSAGE = config.get('UPLOAD_FILE_MESSAGE', 'File uploaded on behalf of WaterButler')
 DELETE_FOLDER_MESSAGE = config.get('DELETE_FOLDER_MESSAGE', 'Folder deleted on behalf of WaterButler')
 
-
 # At some point in the near(?) future git will be changing its internal hash function from SHA-1
 # to SHA-256.  sha1-names are 40 hexdigits long and sha256-names are 64 hexdigits long.  At that
 # point, it seems probable that GitHub will update its API to accept both sha types. When that
@@ -26,3 +25,12 @@ DELETE_FOLDER_MESSAGE = config.get('DELETE_FOLDER_MESSAGE', 'Folder deleted on b
 #   GITHUB_PROVIDER_GITHUB_SHA_LENGTHS=40 64
 #
 GITHUB_SHA_LENGTHS = [int(x) for x in config.get('GITHUB_SHA_LENGTHS', '40').split(' ')]
+
+# The time in seconds to wait before making another attempt to add more tokens
+RL_TOKEN_ADD_DELAY = int(config.get('RL_TOKEN_ADD_DELAY', 1))
+
+# The maximum number of available tokens allowed
+RL_MAX_AVAILABLE_TOKENS = int(config.get('RL_MAX_AVAILABLE_TOKENS', 10))
+
+# The minimum interval in seconds between each request rate update.
+RL_REQ_RATE_UPDATE_INTERVAL = int(config.get('RL_REQ_RATE_UPDATE_INTERVAL', 60))
