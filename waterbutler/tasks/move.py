05bff401c6613426b48567efa4afae5c6da59791
longze chen <cs.longze.chen@gmail.com>
2018-04-04 10:24:27 -0400

    Fix and update move/copy for celery
    - `task_id` should be on dest_provider instead of
      dest_path, probably a typo from the replaced PR
    - Sync `copy()` with `move()` (they should be the
      same) by updating args `request=None` and adding
      a `None` check
    - Update style: reorder imports and fix long line

--- waterbutler/tasks/move.py
+++ waterbutler/tasks/move.py
@@ -1,32 +1,34 @@
 import time
 import logging
 
-from waterbutler.core import utils
 from waterbutler.tasks import core
-from waterbutler.core import remote_logging
 from waterbutler.core.path import WaterButlerPath
+from waterbutler.core import utils, remote_logging
 from waterbutler.core.log_payload import LogPayload
-
 
 logger = logging.getLogger(__name__)
 
 
 @core.celery_task
-async def move(src_bundle, dest_bundle, request: dict=None, start_time=None, **kwargs):
+async def move(src_bundle, dest_bundle, request=None, start_time=None, **kwargs):
+
     request = request or {}
     start_time = start_time or time.time()
 
-    src_path, src_provider = src_bundle.pop('path'), utils.make_provider(**src_bundle.pop('provider'))
-    dest_path, dest_provider = dest_bundle.pop('path'), utils.make_provider(**dest_bundle.pop('provider'))
+    src_path = src_bundle.pop('path')
+    src_provider = utils.make_provider(**src_bundle.pop('provider'))
+    dest_path = dest_bundle.pop('path')
+    dest_provider = utils.make_provider(**dest_bundle.pop('provider'))
 
+    # TODO: should we add `task_id` explicitly as a property for core provider?
     # Save celery task ID. Used to identify provider object running in celery
     src_provider.task_id = move.request.id
-    dest_path.task_id = move.request.id
+    dest_provider.task_id = move.request.id
 
     logger.info('Starting moving {!r}, {!r} to {!r}, {!r}'
                 .format(src_path, src_provider, dest_path, dest_provider))
 
-    metadata, errors = None, []  # type: ignore
+    metadata, errors = None, []
     try:
         metadata, created = await src_provider.move(dest_provider, src_path, dest_path, **kwargs)
     except Exception as e:
