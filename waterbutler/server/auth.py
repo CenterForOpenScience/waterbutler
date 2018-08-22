1d85cddda64cd395d38ffec9e6ccdca294fe701e
Fitz Elliott <fitz@cos.io>
2018-08-22 14:57:46 -0400

    pass path and version to auth handler

--- waterbutler/server/auth.py
+++ waterbutler/server/auth.py
@@ -21,9 +21,12 @@ class AuthHandler:
                 return credential
         raise AuthHandler('no valid credential found')
 
-    async def get(self, resource, provider, request, action=None, auth_type=AuthType.SOURCE):
+    async def get(self, resource, provider, request, action=None, auth_type=AuthType.SOURCE,
+                  path='', version=None):
         for extension in self.manager.extensions:
-            credential = await extension.obj.get(resource, provider, request, action=action, auth_type=auth_type)
+            credential = await extension.obj.get(resource, provider, request,
+                                                 action=action, auth_type=auth_type,
+                                                 path=path, version=version)
             if credential:
                 return credential
         raise AuthHandler('no valid credential found')
