c0c95c729b4fb9884fb1d1b6254f407189d4f9f9
Longze Chen <cslzchen@gmail.com>
2018-09-11 23:24:21 -0400

    Make the maximum retries a provider setting
    During local testing, all 429 failures succeeded upon first retry.
    The issue turned out to be "namespace lock contention" instead of
    "two many requests". It is reasonable to set the default maximum
    value to 2 retires per upload.

--- waterbutler/providers/dropbox/settings.py
+++ waterbutler/providers/dropbox/settings.py
@@ -10,3 +10,5 @@ BASE_CONTENT_URL = config.get('BASE_CONTENT_URL', 'https://content.dropboxapi.co
 CONTIGUOUS_UPLOAD_SIZE_LIMIT = int(config.get('CONTIGUOUS_UPLOAD_SIZE_LIMIT', 150000000))  # 150 MB
 
 CHUNK_SIZE = int(config.get('CHUNK_SIZE', 4000000))  # 4 MB
+
+MAX_429_RETRIES = int(config.get('MAX_429_RETRIES', 2))
