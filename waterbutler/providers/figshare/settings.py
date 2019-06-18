536f66d4b7f1b261a296d1024478a000f2733deb
longze chen <cs.longze.chen@gmail.com>
2019-06-18 06:47:58 -0400

    Refactored Fileset to Dataset for figshare provider
    There are only 2 occurrences where "Fileset" is explicitly used:
    provider init and folder creation. For the former, added Dataset
    and keep Fileset; for the latter, replaced Fileset with Dataset.
    In addition, provider settings are updated accordingly.

--- waterbutler/providers/figshare/settings.py
+++ waterbutler/providers/figshare/settings.py
@@ -7,8 +7,9 @@ BASE_URL = config.get('BASE_URL', 'https://api.figshare.com/v2')
 VIEW_URL = config.get('VIEW_URL', 'https://figshare.com/')
 DOWNLOAD_URL = config.get('VIEW_URL', 'https://ndownloader.figshare.com/')
 
-VALID_CONTAINER_TYPES = ['project', 'collection', 'article', 'fileset']
-# TODO [SVCS-996]: expand the list with more folder-like article types
+# TODO [SVCS-996]: expand all 3 lists below with more folder-like article types
+VALID_CONTAINER_TYPES = ['project', 'collection', 'article', 'dataset', 'fileset']
+ARTICLE_CONTAINER_TYPES = ['article', 'dataset', 'fileset']
 FOLDER_TYPES = [3, 4]  # figshare ID for Dataset (3) and Fileset (4, deprecated)
 
 PRIVATE_IDENTIFIER = 'https://api.figshare.com/v2/account/'
