44874d1eb48852abda57e0eb50e1ccbc028122de
longze chen <cs.longze.chen@gmail.com>
2019-08-08 11:31:00 -0400

    Auth by OSF cookie now bypasses the rate limiter

--- waterbutler/server/api/v1/provider/ratelimiting.py
+++ waterbutler/server/api/v1/provider/ratelimiting.py
@@ -23,8 +23,10 @@ class RateLimitingMixin:
         limit is reached; return False otherwise.
         """
 
-        redis_key = self.get_auth_naive()
-        logger.info('>>> RATE LIMITING >>> KEY >>> {}'.format(redis_key))
+        limit_check, redis_key = self.get_auth_naive()
+        logger.info('>>> RATE LIMITING >>> check={} key={}'.format(limit_check, redis_key))
+        if not limit_check:
+            return False
 
         # TODO: no need to check existence by calling the get since incr works on null keys
         counter = self.redis_conn.get(redis_key)
@@ -44,7 +46,9 @@ class RateLimitingMixin:
         return False
 
     def get_auth_naive(self):
-        """ Get the authentication / authorization credentials from the request.
+        """ Get the obfuscated authentication / authorization credentials from the request.  Return
+        a tuple ``(limit_check, auth_key)`` that tells the rate-limiter 1) whether to rate-limit,
+        and 2) if so, limit by what key.
 
         Refer to ``tornado.httputil.HTTPServerRequest`` for more info on tornado's request object:
             https://www.tornadoweb.org/en/stable/httputil.html#tornado.httputil.HTTPServerRequest
@@ -52,6 +56,15 @@ class RateLimitingMixin:
         This is a NAIVE implementation in which Waterbutler rate-limiter only checks the existence
         of auth creds in the requests without further verifying them with the OSF.  Invalid creds
         will fail the next OSF auth part anyway even if it passes the rate-limiter.
+
+        There are totally four types of auth: 1) OSF cookie, 2) OAuth access token, 3) basic auth w/
+        base64-encoded username/password and 4) w/o any of the three aforementioned auth.  The naive
+        implementation allows cookie auth to bypass the rate-limiter while limiting the rest three.
+
+        Please note that the above exception for cookie auth only applies when it is the only the
+        auth option provided by the request.  The priority order is: token > basic > cookie.
+
+        TODO: check with OSF API auth to see how it deals with multiple auth options.
         """
 
         osf_cookie = bearer_token = basic_creds = None
@@ -68,24 +81,23 @@ class RateLimitingMixin:
             # CASE 3: Requests with basic auth using username and password
             basic_creds = auth_hdrs.split(' ')[1] if auth_hdrs.startswith('Basic ') else None
 
-        # TODO: make sure the remote_ip is the real IP not our load balancers
+        # TODO: Work with DevOps to make sure that the `remote_ip` is the real IP instead of our
+        #       load balancers.  In addition, check relevatn HTTP headers as well.
         # CASE 4: Requests without any expected auth (case 1, 2 or 3 above).
         remote_ip = self.request.remote_ip or 'NOI.PNO.IPN.OIP'
 
-        # TODO: allow cookie auth for free, loosely rate-limit token auth, disable support for basic
-        #       username-password auth and strictly rate-limit requests with no auth.
         if bearer_token:
             logger.info('>>> RATE LIMITING >>> AUTH:TOKEN >>> {}'.format(bearer_token))
-            return 'TOKEN__{}'.format(self._obfuscate_creds(bearer_token))
+            return True, 'TOKEN__{}'.format(self._obfuscate_creds(bearer_token))
         if basic_creds:
             logger.info('>>> RATE LIMITING >>> AUTH:BASIC >>> {}'.format(basic_creds))
-            return 'BASIC__{}'.format(self._obfuscate_creds(basic_creds))
+            return True, 'BASIC__{}'.format(self._obfuscate_creds(basic_creds))
         # SECURITY WARNING: Must check cookie last since it can only be allowed when used alone!
         if osf_cookie:
             logger.info('>>> RATE LIMITING >>> AUTH:COOKIE >>> {}'.format(osf_cookie))
-            return 'COOKIE_{}'.format(self._obfuscate_creds(osf_cookie))
+            return False, 'COOKIE_{}'.format(self._obfuscate_creds(osf_cookie))
         logger.info('>>> RATE LIMITING >>> AUTH:NONE >>> {}'.format(remote_ip))
-        return 'NOAUTH_{}'.format(self._obfuscate_creds(remote_ip))
+        return True, 'NOAUTH_{}'.format(self._obfuscate_creds(remote_ip))
 
     @staticmethod
     def _obfuscate_creds(creds):
