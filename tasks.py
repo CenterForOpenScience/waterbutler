94c12958921c55b0e0cfc00432a243a62f165f81
Fitz Elliott <fitz@cos.io>
2019-10-07 23:23:10 -0400

    remove old pin on certifi
     * The python module `certifi` was pinned to a very old version to
       work around issues with Rackspace.  It has been 4+ years since those
       issues were encountered, and perhaps it's time to retire them.

--- tasks.py
+++ tasks.py
@@ -5,29 +5,11 @@ from invoke import task
 WHEELHOUSE_PATH = os.environ.get('WHEELHOUSE')
 
 
-def monkey_patch(ctx):
-    # Force an older cacert.pem from certifi v2015.4.28, prevents an ssl failure w/ identity.api.rackspacecloud.com.
-    #
-    # SubjectAltNameWarning: Certificate for identity.api.rackspacecloud.com has no `subjectAltName`, falling
-    # back to check for a `commonName` for now. This feature is being removed by major browsers and deprecated by
-    # RFC 2818. (See  https://github.com/shazow/urllib3/issues/497  for details.)
-    # SubjectAltNameWarning
-    import ssl
-    import certifi
-
-    _create_default_context = ssl.create_default_context
-
-    def create_default_context(purpose=ssl.Purpose.SERVER_AUTH, *, cafile=None, capath=None, cadata=None):
-        if cafile is None:
-            cafile = certifi.where()
-        return _create_default_context(purpose=purpose, cafile=cafile, capath=capath, cadata=cadata)
-    ssl.create_default_context = create_default_context
-
-
 @task
 def wheelhouse(ctx, develop=False, pty=True):
     req_file = 'dev-requirements.txt' if develop else 'requirements.txt'
-    cmd = 'pip wheel --find-links={} -r {} --wheel-dir={}'.format(WHEELHOUSE_PATH, req_file, WHEELHOUSE_PATH)
+    cmd = 'pip wheel --find-links={} -r {} --wheel-dir={}'.format(WHEELHOUSE_PATH, req_file,
+                                                                  WHEELHOUSE_PATH)
     ctx.run(cmd, pty=pty)
 
 
@@ -53,7 +35,8 @@ def flake(ctx):
 @task
 def mypy(ctx):
     """
-    Check python types using mypy (additional level of linting). Follows options defined in setup.cfg
+    Check python types using mypy (additional level of linting). Follows options defined in
+    setup.cfg
     """
     ctx.run('mypy waterbutler/', pty=True)
 
@@ -101,7 +84,6 @@ def test(ctx, verbose=False, types=False, nocov=False, provider=None, path=None)
 
 @task
 def celery(ctx, loglevel='INFO', hostname='%h'):
-    monkey_patch(ctx)
 
     from waterbutler.tasks.app import app
     command = ['worker']
@@ -119,13 +101,13 @@ def rabbitmq(ctx):
 
 @task
 def server(ctx):
-    monkey_patch(ctx)
 
     if os.environ.get('REMOTE_DEBUG', None):
         import pydevd
         # e.g. '127.0.0.1:5678'
         remote_parts = os.environ.get('REMOTE_DEBUG').split(':')
-        pydevd.settrace(remote_parts[0], port=int(remote_parts[1]), suspend=False, stdoutToServer=True, stderrToServer=True)
+        pydevd.settrace(remote_parts[0], port=int(remote_parts[1]), suspend=False,
+                        stdoutToServer=True, stderrToServer=True)
 
     from waterbutler.server.app import serve
     serve()
