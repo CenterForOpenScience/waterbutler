d7afa573ac73ae4526e31a43d621a9c9dee6c9ee
yacchin1205 <yazawa@yzwlab.net>
2021-07-25 13:58:23 +0900

    Fix syntax

--- waterbutler/providers/onedrivebusiness/provider.py
+++ waterbutler/providers/onedrivebusiness/provider.py
@@ -1,5 +1,4 @@
 import logging
-import json
 
 from waterbutler.providers.onedrive import OneDriveProvider
 
@@ -12,6 +11,4 @@ class OneDriveBusinessProvider(OneDriveProvider):
 
     def __init__(self, auth, credentials, settings, **kwargs):
         super().__init__(auth, credentials, settings, **kwargs)
-        #self.admin_dbmid = self.settings['admin_dbmid']
-        #self.team_folder_id = self.settings['team_folder_id']
         logger.info('settings: {}'.format(settings))
