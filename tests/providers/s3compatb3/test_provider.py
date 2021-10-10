17ce6d71a0106cfbbce3ad3c3008f207969c7897
hide24 <hidetoshi.yoshimoto@gmail.com>
2021-10-10 17:32:15 +0900

    update test_s3compatb3porvider

--- tests/providers/s3compatb3/test_provider.py
+++ tests/providers/s3compatb3/test_provider.py
@@ -767,7 +767,7 @@ class TestMetadata:
     async def test_upload(self, provider, file_content, file_stream, file_metadata, mock_time, file_metadata_object):
         path = WaterButlerPath('/foobah', prepend=provider.prefix)
         content_md5 = hashlib.md5(file_content).hexdigest()
-        mock_metadata = mock.MagicMock(side_effect=[exceptions.MetadataError(str(path.full_path), code=404), S3CompatB3FileMetadata(provider, file_metadata_object)])
+        mock_metadata = mock.AsyncMock(side_effect=[exceptions.MetadataError(str(path.full_path), code=404), S3CompatB3FileMetadata(provider, file_metadata_object)])
         provider.metadata = mock_metadata
         # url = provider.bucket.new_key(path.full_path).generate_url(100, 'PUT')
         # metadata_url = provider.bucket.new_key(path.full_path).generate_url(100, 'HEAD')
