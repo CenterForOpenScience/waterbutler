d9823648a05092b9b64ed465a56671a35fa6514c
mkovalua <mkoval@exoft.net>
2025-04-18 16:48:07 +0300

    Update create.py

--- waterbutler/server/api/v1/provider/create.py
+++ waterbutler/server/api/v1/provider/create.py
@@ -109,7 +109,7 @@ class CreateMixin:
         self.metadata, created = await self.uploader
         self.writer.close()
         # Docs: https://docs.python.org/3/library/asyncio-stream.html#asyncio.StreamWriter.wait_closed
-        await writer.wait_closed()
+        await self.writer.wait_closed()
         self.wsock.close()
         self.rsock.close()
         self.rfd.close()
