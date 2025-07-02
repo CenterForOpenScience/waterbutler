99b21f62f0802db461dafddb4b334fb8ad730bdb
Fitz Elliott <fitz@cos.io>
2025-07-02 07:49:44 -0400

    Fix WB serialization for kombu/celery
     * Specifically target only subclasses of WaterButlerPath,
       BaseMetadata, and BaseFileRevisionMetadata.
    
     * Add dehydrate() and rehydrate() methods to all *Metadata objects to
       allow custom serialization to a json-compatible format.

--- waterbutler/providers/gitlab/metadata.py
+++ waterbutler/providers/gitlab/metadata.py
@@ -15,6 +15,17 @@ class BaseGitLabMetadata(metadata.BaseMetadata):
         super().__init__(raw)
         self._path_obj = path
 
+    def _dehydrate(self):
+        payload = super()._dehydrate()
+        payload['_path_obj'] = self._path_obj
+        return payload
+
+    @classmethod
+    def _rehydrate(cls, payload):
+        args = super()._rehydrate(payload)
+        args.append(payload['_path_obj'])
+        return args
+
     @property
     def provider(self) -> str:
         return 'gitlab'
@@ -84,6 +95,21 @@ class GitLabFileMetadata(BaseGitLabMetadata, metadata.BaseFileMetadata):
         self.host = host
         self.owner = owner
         self.repo = repo
+
+    def _dehydrate(self):
+        payload = super()._dehydrate()
+        payload['_path_obj'] = self._path_obj
+        payload['host'] = self.host
+        payload['owner'] = self.owner
+        payload['repo'] = self.repo
+        return payload
+
+    @classmethod
+    def _rehydrate(cls, payload):
+        args = super()._rehydrate(payload)
+        args.append(payload['_path_obj'], host=payload['host'],
+                    owner=payload['owner'], repo=payload['repo'])
+        return args
 
     @property
     def modified(self) -> str:
